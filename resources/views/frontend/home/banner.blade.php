c65b64f1eb3619653506df109f22376cf808e59d
alireza fard <alssfard@gmail.com>
2023-09-23 20:30:24 +0330

    #

--- resources/views/frontend/home/banner.blade.php
+++ resources/views/frontend/home/banner.blade.php
@@ -13,107 +13,67 @@ $ptypes = App\Models\PropertyType::latest()->get();
 <section style="background-color: black" class="banner-section">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-7">
+            <div class="col-12">
                 <div class="content-box text-center">
                     <h2 class="text-warning">گروه مشاورین املاک ملک گستر</h2>
                     <hr class="hr text-white">
                     <h4 class="text-warning">بهترین املاک شهر و محل اقامت خود را پیدا کنید</h4>
                     <br>
                 </div>
-                <div class="search-field">
-                    <div class="tabs-box">
-                        <div class="tab-btn-box">
-                            <ul class="tab-btns tab-buttons centred clearfix d-flex justify-content-center">
-                                <li class="tab-btn active-btn" data-tab="#tab-1">خرید و فروش</li>
-                                <li class="tab-btn" data-tab="#tab-2">رهن و اجاره</li>
-                            </ul>
-                        </div>
-                        <div class="tabs-content info-group">
-                            <div class="tab active-tab" id="tab-1">
-                                <div class="inner-box">
-                                    <div class="top-search rounded-pill">
-                                        <form action="{{ route('buy.property.search') }}" method="post"
-                                            class="search-form">
-                                            @csrf
-                                            <div class="row">
-                                                <div class="col-lg-5 col-md-12 mb-3 mb-lg-0">
-                                                    <div class="form-group">
-                                                        <div class="field-input">
-                                                            <i class="fas fa-search"></i>
-                                                            <input class="form-control rounded-pill" type="search"
-                                                                name="search" style="padding-right: 5px"
-                                                                placeholder="اطلاعات ملک را وارد فرمایید" required="">
-                                                        </div>
+            </div>
+        </div>
+    </div>
+    <div class="search-field">
+        <div class="tabs-box">
+            <div class="tab-btn-box">
+                <ul class="tab-btns tab-buttons">
+                    <li class="tab-btn active-btn" data-tab="#tab-1">خرید و فروش</li>
+                    <li class="tab-btn" data-tab="#tab-2">رهن و اجاره</li>
+                </ul>
+            </div>
+            <div class="tab active-tab" id="tab-1">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-lg-4">
+                            <div class="top-search rounded-pill
+                        ">
+                                <form action="search.php" method="post">
+                                    @csrf
+                                    <div class="d-flex justify-content-center align-items-center">
+                                        <ul class="col-md-6 col-lg-4 d-flex" style="flex-wrap: nowrap;">
+                                            <li>
+                                                <div class="form-group">
+                                                    <div class="field-input">
+                                                        <i class="fas fa-search"></i>
+                                                        <input class="form-control rounded-pill" type="search"
+                                                            name="search" placeholder="اطلاعات ملک را وارد فرمایید"
+                                                            required>
                                                     </div>
                                                 </div>
-                                                <div
-                                                    class="col-lg-3 col-md-6 mb-3 mb-lg-0 d-flex justify-content-center">
-                                                    <div class="form-group">
-                                                        <div class="select-box">
-                                                            <select name="pType_id" class="form-control rounded-pill">
-                                                                <option>لیست املاک</option>
-                                                                @foreach ($ptypes as $type)
-                                                                <option value="{{ $type->type_name }}">{{
-                                                                    $type->type_name }}</option>
-                                                                @endforeach
-                                                            </select>
-                                                        </div>
+                                            </li>
+                                            <li>
+                                                <div class="form-group">
+                                                    <div class="select-box">
+                                                        <select name="pType_id" class="form-control rounded-pill">
+                                                            <option>لیست املاک</option>
+                                                            @foreach ($ptypes as $type)
+                                                            <option value="{{ $type->type_name }}">{{ $type->type_name
+                                                                }}
+                                                            </option>
+                                                            @endforeach
+                                                        </select>
                                                     </div>
                                                 </div>
-                                                <div class="col-lg-4 col-md-6 d-flex justify-content-center">
-                                                    <div class="form-group">
-                                                        <button style="font-size: 14px;font-weight:bold" type="submit"
-                                                            class="btn btn-primary btn-lx rounded-pill">جست و
-                                                            جو</button>
-                                                    </div>
+                                            </li>
+                                            <li>
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary rounded-pill">جست و
+                                                        جو</button>
                                                 </div>
-                                            </div>
-                                        </form>
+                                            </li>
+                                        </ul>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="tab" id="tab-2">
-                                <div class="inner-box">
-                                    <div class="top-search">
-                                        <form action="{{ route('rent.property.search') }}" method="post"
-                                            class="search-form">
-                                            @csrf
-                                            <div class="row">
-                                                <div class="col-lg-5 col-md-12 mb-3 mb-lg-0">
-                                                    <div class="form-group">
-                                                        <div class="field-input">
-                                                            <i class="fas fa-search"></i>
-                                                            <input class="form-control rounded-pill" type="search"
-                                                                name="search" style="padding-right: 5px"
-                                                                placeholder="اطلاعات ملک را وارد فرمایید" required="">
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div
-                                                    class="col-lg-3 col-md-6 mb-3 mb-lg-0 d-flex justify-content-center">
-                                                    <div class="form-group">
-                                                        <div class="select-box">
-                                                            <select name="pType_id" class="form-control rounded-pill">
-                                                                <option>لیست املاک</option>
-                                                                @foreach ($ptypes as $type)
-                                                                <option value="{{ $type->type_name }}">{{
-                                                                    $type->type_name }}</option>
-                                                                @endforeach
-                                                            </select>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="col-lg-4 col-md-6 d-flex justify-content-center">
-                                                    <div class="form-group">
-                                                        <button style="font-size: 14px;font-weight:bold" type="submit"
-                                                            class="btn btn-primary btn-lx rounded-pill">جست و
-                                                            جو</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
--- resources/views/frontend/home/category.blade.php
+++ resources/views/frontend/home/category.blade.php
@@ -1,62 +1,48 @@
-@php
-$ptype = App\Models\PropertyType::latest()
-->limit(5)
-->get();
-@endphp
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-<style>
-    .underline-on-hover:hover {
-        text-decoration: underline;
-    }
-</style>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 text-center">
-            <section class="category-section centered">
-                <div class="auto-container">
-                    <div class="sec-title centered">
-                        <h3 style="color: #FFB616;" class="fw-bold ml-4 mr-4 border p-2 rounded-pill bg-color-2">خدمات ما</h3>
-                    </div>
-                    <div class="inner-container wow slideInLeft animated" data-wow-delay="00ms" data-wow-duration="800ms">
-                        <ul class="category-list clearfix">
-                            @php
-                            $icons = ['fa fa-newspaper', 'fa fa-edit', 'fa fa-plus', 'fa fa-building', 'fa fa-home'];
-                            $text = ['مقالات', 'کارشناسی املاک', 'ثبت ملک', 'درخواست ملک', 'بانک املاک'];
-                            $link1 = [route('blog.list'), route('Index.kojanajafabad'), route('formForUsShow'), route('ShowRequestForProperty.ShowRequest'), route('FrontEndAllTypes.index')];
-                            @endphp
-                            <div style="align-items: center;justify-content: center;" class="row">
-                            @for ($i = 0; $i < count($icons); $i++)
-                                <div class="col-md-2">
-                                    <div class="category-block-one">
-                                        <div class="position-relative">
-                                            <img style="border-radius: 25px;" src="upload/Category.jpg" alt="Category Image" class="img-fluid p-2 rounded-lg">
-                                            <a style="color:black;text-decoration: none;" href="{{ $link1[$i] }}" class="position-absolute top-50 start-50 translate-middle text-center">
-                                                <i class="{{ $icons[$i] }}" style="font-size: 20px; color:black"></i>
-                                                <br>
-                                                <h6 style="font-weight: bold;font-size: 12px" class="underline-on-hover">{{ $text[$i] }}</h6>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endfor
-                            </div>
-                        </ul>
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="more-btn"><a href="{{ route('rent.property') }}" class="btn btn-primary">لیست کامل املاک برای رهن</a></div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="more-btn"><a href="{{ route('FrontEndAllTypes.index') }}" class="btn btn-primary">لیست کامل املاک و طبقه بندی ها</a></div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="more-btn"><a href="{{ route('buy.property') }}" class="btn btn-primary">لیست کامل املاک برای خرید</a></div>
-                                </div>
+<div class="row col-12 text-center">
+    <section class="category-section centered">
+        <div class="sec-title centered">
+            <h3 style="color: #FFB616;" class="fw-bold ml-4 mr-4 border p-2 rounded-pill bg-color-2">خدمات
+                ما</h3>
+        </div>
+        <div class="inner-container wow slideInLeft animated" data-wow-delay="00ms" data-wow-duration="800ms">
+            <ul class="category-list clearfix">
+                @php
+                $img = ['upload\category\مقالات.jpg', 'upload\category\کارشناسی املاک.jpg',
+                'upload\category\ثبت ملک.jpg', 'upload\category\درخواست ملک.jpg',
+                'upload\category\بانک املاک.jpg'];
+                $link = [route('blog.list'), route('Index.kojanajafabad'), route('formForUsShow'),
+                route('ShowRequestForProperty.ShowRequest'), route('FrontEndAllTypes.index')];
+                @endphp
+                <div style="align-items: center;justify-content: center;" class="row">
+                    @for ($i = 0; $i < count($img); $i++) <div class="col-md-2">
+                        <div class="category-block-one">
+                            <div class="position-relative">
+                                <a href="{{ $link[$i] }}">
+                                    <img src="{{ $img[$i] }}" alt="Category Image" class="img-fluid rounded-lg p-1"
+                                        style="border-radius: 25px;">
+                                </a>
                             </div>
                         </div>
-                    </div>
                 </div>
-            </section>
+                @endfor
         </div>
-    </div>
+        </ul>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="more-btn"><a href="{{ route('rent.property') }}" class="btn btn-primary">لیست
+                            کامل املاک برای رهن</a></div>
+                </div>
+                <div class="col-md-4">
+                    <div class="more-btn"><a href="{{ route('FrontEndAllTypes.index') }}" class="btn btn-primary">لیست
+                            کامل املاک و طبقه بندی ها</a></div>
+                </div>
+                <div class="col-md-4">
+                    <div class="more-btn"><a href="{{ route('buy.property') }}" class="btn btn-primary">لیست
+                            کامل املاک برای خرید</a></div>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
